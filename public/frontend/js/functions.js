76c6c32d8830f655d409e608e21dc9bb096b841e
amr-fadl <amrfa377@gmail.com>
2022-04-10 13:29:07 +0200

    minify js and css

--- public/frontend/js/functions.js
+++ public/frontend/js/functions.js
@@ -1,10 +1,1 @@
-!(function (a) {
-    "use strict";
-    a(window).on("load", function () {
-        a('[data-loader="circle-side"]').fadeOut(),
-            a("#preloader").delay(350).fadeOut("slow"),
-            a("body").delay(350),
-            a(".left_title").addClass("start_animation"),
-            a("#sub_content_in").addClass("start_animation");
-    })
-})(window.jQuery);
+!function(a){"use strict";a(window).on("load",function(){a('[data-loader="circle-side"]').fadeOut(),a("#preloader").delay(350).fadeOut("slow"),a("body").delay(350),a(".left_title").addClass("start_animation"),a("#sub_content_in").addClass("start_animation")})}(window.jQuery);
